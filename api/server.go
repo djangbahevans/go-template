a7a4a130f6e0485a193b33ff9b938c8e5647648d
Evans Djangbah <djangbahevans@yahoo.com>
2024-05-28 10:19:25 +0000

    chore: Add health check endpoint to API server

--- api/server.go
+++ api/server.go
@@ -7,6 +7,7 @@ import (
 	v1 "github.com/djangbahevans/go-template/api/v1"
 	"github.com/djangbahevans/go-template/middleware"
 	"github.com/djangbahevans/go-template/services"
+	"github.com/djangbahevans/go-template/utils"
 )
 
 type ApiServer struct {
@@ -21,9 +22,10 @@ func NewApiServer(addr string, userService services.UserService) *ApiServer {
 func (s *ApiServer) Start() error {
 	r := http.NewServeMux()
 	v1.RegisterV1Routes(r, s.userService)
-
+	
 	api := http.NewServeMux()
 	api.Handle("/api/", http.StripPrefix("/api", r))
+	api.HandleFunc("/health", HealthCheck)
 
 	server := &http.Server{
 		Addr:    s.addr,
@@ -33,4 +35,8 @@ func (s *ApiServer) Start() error {
 	log.Printf("server listening on %s", s.addr)
 
 	return server.ListenAndServe()
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	utils.RespondJSON(w, map[string]string{"status": "ok"})
 }
