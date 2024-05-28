48f6c02f7fd389fa8cf9d9436bd9e264217707bd
Evans Djangbah <djangbahevans@yahoo.com>
2024-05-28 10:49:44 +0000

    chore: Update logging middleware to include status code and request duration

--- middleware/logging.go
+++ middleware/logging.go
@@ -6,10 +6,21 @@ import (
 	"time"
 )
 
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *statusWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
+		sw := statusWriter{ResponseWriter: w, status: http.StatusOK}
+		next.ServeHTTP(&sw, r)
+		log.Printf("%s %s %s %d %s", r.RemoteAddr, r.Method, r.URL.Path, sw.status, time.Since(start))
 	})
 }
